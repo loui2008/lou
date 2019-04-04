ec206ff199075eb4f8a0e804e619a0011a0524a9
loui2008 <loui2008@163.com>
2019-04-04 23:26:04 +0800

    添加创建文件夹
    添加创建文件夹

--- scp.bash
+++ scp.bash
@@ -4,11 +4,12 @@ day=`date -d 'yesterday' +%m%d`
 for station in `seq 50001 50099`
 do 
 	##PWD=`cd /record/record_A$i/$year/$day/$station`
+	ssh root@192.168.46.137 mkdir /record/record_A$i/$year/$day/
 	for i in `seq 1 4`
 	do
 		if [ -d /record/record_A$i/$year/$day/$station ] ; then
 		echo "/record/record_A$i/$year/$day/$station"
-		##scp -l 5000 -r /record/record_A$i/$year/$day/$station root@10.118.5.180:/record/record_A$i/$year/$day/$station
+		scp -l 5000 -r /record/record_A$i/$year/$day/$station root@192.168.46.137:/record/record_A$i/$year/$day/$station
 		fi
 	done
 done 
