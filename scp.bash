71c401c1922e39787e42d13bc74c0489f07d0f5f
loui2008 <loui2008@163.com>
2019-04-05 09:59:56 +0800

    v1.0完成
    v1.0完成

--- scp.bash
+++ scp.bash
@@ -1,15 +1,17 @@
-#!/bin/bash
+#/bin/bash
+##copyright:lou
+##version:1.0
 year=`date +%Y`
-day=`date -d 'yesterday' +%m%d`
-for station in `seq 50001 50099`
-do 
-	##PWD=`cd /record/record_A$i/$year/$day/$station`
-	ssh root@192.168.46.137 mkdir /record/record_A$i/$year/$day/
+day=`date -d 'yesterday'+%m%d`
+IP='192.168.46.134'
+for station in `seq 50001 50006`
+do
 	for i in `seq 1 4`
 	do
-		if [ -d /record/record_A$i/$year/$day/$station ] ; then
-		echo "/record/record_A$i/$year/$day/$station"
-		scp -l 5000 -r /record/record_A$i/$year/$day/$station root@192.168.46.137:/record/record_A$i/$year/$day/$station
-		fi
+	ssh root@$IP "mkdir -p /record/record_A$i/$year/$day/"
+	if [ -d /record/record_A$i]/$year/$day/$station ] ; then
+	echo "/record/record_A$i/$year/$day/$station"
+	scp -l 5000 -r  /record/record_A$i/$year/$day/$station root@$IP:/record/record_A$i/$year/$day/$station
+	fi
 	done
-done 
+done
