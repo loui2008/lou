a51fb5f701a00e027e8ca7df7bee5e855cac757b
loui2008 <loui2008@163.com>
2019-04-07 15:42:21 +0800

    更新
    更新

--- ftp.bash
+++ ftp.bash
@@ -1,10 +1,26 @@
 #!/bin/bash
-##maker:lou
-##version:1.0
-IP=""
-PORT=""
-USER=""
-PASSWD=""
-REMOTE_DIR=""
-LOCAL_DIR=""
-DEST_FILE=""
+##version=1.0
+##makeer=lou
+IP="192.168.2.100"
+PORT="21"
+USER="110"
+PASSWORD="110"
+YEAR=`date +%Y`
+DATE=`date -d "yesterday" +%m%d`
+for station in `seq 50001 50006`
+do
+	for i in `seq 1 4`
+	do
+	ftp  -i -n << EOF
+		open $IP $PORT
+		user $USER $PASSWORD
+		bin
+		mkdir /record/record_A$i/$YEAR/$DATE/%station
+		cd /record/record_A$i/$YEAR/$DATE/%station
+		lcd /record/record_A$i/$YEAR/$DATE/%station
+		mput *
+		close
+		quit
+		EOF
+	done
+done
