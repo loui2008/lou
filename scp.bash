0a3511a3762b30c7c4cc635d2a83b4a34efa586b
loui2008 <loui2008@163.com>
2019-04-10 11:41:22 +0800

    添加报错和目录存在输出
    添加报错和目录存在输出

--- scp.bash
+++ scp.bash
@@ -9,9 +9,9 @@ do
 	for i in `seq 1 4`
 	do
 	if [ -d "/record/record_A$i/$year/$day/$station" ] ; then
-	echo "/record/record_A$i/$year/$day/$station"
-	ssh root@$IP mkdir -p /record/record_A$i/$year/$day/
-	scp -l 5000 -r  /record/record_A$i/$year/$day/$station root@$IP:/record/record_A$i/$year/$day/$station
+	echo "dir is exist :/record/record_A$i/$year/$day/$station">>file.txt
+	ssh root@$IP mkdir -p /record/record_A$i/$year/$day/ >>/home/log.txt 2>&1
+	scp -l 5000 -r  /record/record_A$i/$year/$day/$station root@$IP:/record/record_A$i/$year/$day/$station  >>/home/log.txt 2>&1
 	fi
 	done
 done
